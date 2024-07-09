7393e906fa1e882b5ec14f1296b0f336b962a9e5
Alex Pena <alexhellhammer@hotmail.com>
2024-07-08 21:59:02 -0500

    creacion de modelos

--- src/main/java/com/libralex/literalura/LiteraluraApplication.java
+++ src/main/java/com/libralex/literalura/LiteraluraApplication.java
@@ -1,5 +1,6 @@
 package com.libralex.literalura;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,15 +10,17 @@ import java.security.Principal;
 @SpringBootApplication
 public class LiteraluraApplication implements CommandLineRunner {
 
+	@Autowired
+	private AutorRepository autorRepository;
+	@Autowired
+	private LibroRepository libroRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(LiteraluraApplication.class, args);
 	}
 
-
-
 	@Override
 	public void run(String... args) throws Exception {
-		Principal principal = new Principal(autoRepository, libroRepository);
+		Principal principal = new Principal(autorRepository, libroRepository);
 		principal.mostrarMenu();
 	}
 }
--- src/main/java/model/DatosAutor.java
+++ src/main/java/model/DatosAutor.java
@@ -1,7 +1,9 @@
 package model;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record DatosAutor(
 
         @JsonAlias("name") String nombre,
